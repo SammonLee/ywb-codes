a69e769608ca686b5146be01a494d0ed465590ab
wenbinye <wenbinye@01fcc042-0321-11de-b556-9305e745af9e>
2009-03-26 06:31:20 +0000

    day 07

---
+++ php/jobeet/apps/frontend/modules/category/actions/actions.class.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * category actions.
+ *
+ * @package    jobeet
+ * @subpackage category
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
+ */
+class categoryActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+  public function executeShow(sfWebRequest $request)
+  {
+    $this->category = $this->getRoute()->getObject();
+    $this->pager = new sfPropelPager(
+      'JobeetJob',
+      sfConfig::get('app_max_jobs_on_category')
+    );
+    $this->pager->setCriteria($this->category->getActiveJobsCriteria());
+    $this->pager->setPage($request->getParameter('page', 1));
+    $this->pager->init();
+  }
+}
--- php/jobeet/lib/model/JobeetJobPeer.php
+++ php/jobeet/lib/model/JobeetJobPeer.php
@@ -2,4 +2,39 @@
 
 class JobeetJobPeer extends BaseJobeetJobPeer
 {
+  static public function getActiveJobs(Criteria $criteria = null)
+  {
+    if (is_null($criteria))
+    {
+      $criteria = new Criteria();
+    }
+    $criteria->add(self::EXPIRES_AT, time(), Criteria::GREATER_THAN);
+    $criteria->addDescendingOrderByColumn(self::EXPIRES_AT); 
+    return self::doSelect($criteria);
+  }
+
+  static public function doSelectActive(Criteria $criteria)
+  {
+    $criteria->add(JobeetJobPeer::EXPIRES_AT, time(), Criteria::GREATER_THAN);
+ 
+    return self::doSelectOne($criteria);
+  }
+
+  static public function countActiveJobs(Criteria $criteria = null)
+  {
+    return self::doCount(self::addActiveJobsCriteria($criteria));
+  }
+ 
+  static public function addActiveJobsCriteria(Criteria $criteria = null)
+  {
+    if (is_null($criteria))
+    {
+      $criteria = new Criteria();
+    }
+ 
+    $criteria->add(self::EXPIRES_AT, time(), Criteria::GREATER_THAN);
+    $criteria->addDescendingOrderByColumn(self::CREATED_AT);
+ 
+    return $criteria;
+  }
 }
