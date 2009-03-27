39717214e83a5d301c3de9e41d8dc987faf8d165
wenbinye <wenbinye@01fcc042-0321-11de-b556-9305e745af9e>
2009-03-27 06:02:26 +0000

    day 10

--- trunk/php/jobeet/apps/frontend/modules/job/templates/showSuccess.php
+++ trunk/php/jobeet/apps/frontend/modules/job/templates/showSuccess.php
@@ -1,11 +1,8 @@
-<!-- apps/frontend/modules/job/templates/showSuccess.php -->
-<?php slot('title') ?>
-  <?php echo sprintf('%s is looking for a %s', $job->getCompany(), $job->getPosition()) ?>
-<?php end_slot(); ?>
 <?php use_stylesheet('job.css') ?>
+<?php slot('title', sprintf('%s is looking for a %s', $job->getCompany(), $job->getPosition())) ?>
 <?php use_helper('Text') ?>
-
-<?php if ($sf_request->getParameter('token') == $job->getToken() ): ?>
+ 
+<?php if ($sf_request->getParameter('token') == $job->getToken()): ?>
   <?php include_partial('job/admin', array('job' => $job)) ?>
 <?php endif; ?>
  
@@ -20,8 +17,7 @@
   <?php if ($job->getLogo()): ?>
     <div class="logo">
       <a href="<?php echo $job->getUrl() ?>">
-        <img src="/uploads/jobs/<?php echo $job->getLogo() ?>"
-          alt="<?php echo $job->getCompany() ?> logo" />
+        <img src="<?php echo $job->getLogo() ?>" alt="<?php echo $job->getCompany() ?> logo" />
       </a>
     </div>
   <?php endif; ?>
@@ -37,10 +33,5 @@
   <div class="meta">
     <small>posted on <?php echo $job->getCreatedAt('m/d/Y') ?></small>
   </div>
- 
-  <div style="padding: 20px 0">
-    <a href="<?php echo url_for('job_edit', $job) ?>">
-      Edit
-    </a>
-  </div>
+
 </div>
