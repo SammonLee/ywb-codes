1c8867827a80712a8380eb9229853831e5bd5bf7
wenbinye <wenbinye@01fcc042-0321-11de-b556-9305e745af9e>
2009-04-08 16:00:30 +0000

    finish Net::Top::Request::Item

---
+++ trunk/TOP_API/perl/lib/Net/Top/Request/Item.pm
@@ -0,0 +1,74 @@
+package Net::Top::Request::Item;
+use base 'Net::Top::Request::Factory';
+sub get {
+   my $pkg = shift;
+   return $pkg->factory('Net::Top::Request::Item::Get', @_);
+}
+
+package Net::Top::Request::Item::Get;
+use base 'Net::Top::Request';
+
+__PACKAGE__->make_request(
+  'fields' => {
+    ':small' => [
+      'iid',
+      'title',
+      'nick',
+      'type',
+      'cid',
+      'num',
+      'price'
+    ],
+    ':image' => [
+      'pic_path',
+      'itemimg',
+      'propimg'
+    ],
+    ':all' => [
+      ':large',
+      ':image',
+      'increment',
+      'has_discount',
+      'has_invoice',
+      'has_warranty',
+      'has_showcase',
+      'auto_repost',
+      'auction_point'
+    ],
+    ':large' => [
+      ':small',
+      ':postage',
+      'props',
+      'property_alias',
+      'desc',
+      'seller_cids',
+      'valid_thru',
+      'list_time',
+      'delist_time',
+      'stuff_status',
+      'location',
+      'modified',
+      'sku',
+      'approve_status',
+      'product_id'
+    ],
+    ':postage' => [
+      'post_fee',
+      'express_fee',
+      'ems_fee',
+      'postage_id',
+      'freight_payer'
+    ]
+  },
+  'optional_params' => [
+    'format'
+  ],
+  'api_method' => 'taobao.item.get',
+  'require_params' => [
+    'fields',
+    'nick',
+    'iid'
+  ]
+);
+
+1;
