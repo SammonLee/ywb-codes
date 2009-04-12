cc66ab430ba950309baf44874db1791b98e142bd
wenbinye <wenbinye@01fcc042-0321-11de-b556-9305e745af9e>
2009-04-12 01:41:10 +0000

    new api

---
+++ TOP_API/perl/lib/Net/Top/Request/Base/Item.pm
@@ -0,0 +1,64 @@
+package Net::Top::Request::Base::Item::Get;
+use base 'Net::Top::Request';
+
+__PACKAGE__->make_request(
+    'fields' => {
+        ':small' => [
+            'iid',
+            'title',
+            'nick',
+            'type',
+            'cid',
+            'num',
+            'price'
+        ],
+        ':image' => [
+            'pic_path',
+            'itemimg',
+            'propimg'
+        ],
+        ':all' => [
+            ':large',
+            ':image',
+            'increment',
+            'has_discount',
+            'has_invoice',
+            'has_warranty',
+            'has_showcase',
+            'auto_repost',
+            'auction_point'
+        ],
+        ':large' => [
+            ':small',
+            ':postage',
+            'props',
+            'property_alias',
+            'desc',
+            'seller_cids',
+            'valid_thru',
+            'list_time',
+            'delist_time',
+            'stuff_status',
+            'location',
+            'modified',
+            'sku',
+            'approve_status',
+            'product_id'
+        ],
+        ':postage' => [
+            'post_fee',
+            'express_fee',
+            'ems_fee',
+            'postage_id',
+            'freight_payer'
+        ]
+    },
+    'list_tags' => [ 'sku', 'itemimg', 'propimg' ],
+    'api_method' => 'taobao.item.get',
+    'require_params' => [
+        'fields',
+        'nick',
+        'iid'
+    ]
+);
+1;
