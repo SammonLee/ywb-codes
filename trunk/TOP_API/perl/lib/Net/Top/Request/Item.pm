4a5d26b4ec74f954108e5368d2de44615b97db8f
wenbinye <wenbinye@01fcc042-0321-11de-b556-9305e745af9e>
2009-04-12 01:41:10 +0000

    new api

--- trunk/TOP_API/perl/lib/Net/Top/Request/Item.pm
+++ trunk/TOP_API/perl/lib/Net/Top/Request/Item.pm
@@ -1,3 +1,5 @@
+use Net::Top::Request::Base::Item;
+
 package Net::Top::Request::Item;
 sub get {
     my $pkg = shift;
@@ -5,67 +7,7 @@ sub get {
 }
 
 package Net::Top::Request::Item::Get;
-use base 'Net::Top::Request';
 
-__PACKAGE__->make_request(
-    'fields' => {
-        ':small' => [
-            'iid',
-            'title',
-            'nick',
-            'type',
-            'cid',
-            'num',
-            'price'
-        ],
-        ':image' => [
-            'pic_path',
-            'itemimg',
-            'propimg'
-        ],
-        ':all' => [
-            ':large',
-            ':image',
-            'increment',
-            'has_discount',
-            'has_invoice',
-            'has_warranty',
-            'has_showcase',
-            'auto_repost',
-            'auction_point'
-        ],
-        ':large' => [
-            ':small',
-            ':postage',
-            'props',
-            'property_alias',
-            'desc',
-            'seller_cids',
-            'valid_thru',
-            'list_time',
-            'delist_time',
-            'stuff_status',
-            'location',
-            'modified',
-            'sku',
-            'approve_status',
-            'product_id'
-        ],
-        ':postage' => [
-            'post_fee',
-            'express_fee',
-            'ems_fee',
-            'postage_id',
-            'freight_payer'
-        ]
-    },
-    'list_tags' => [ 'sku', 'itemimg', 'propimg' ],
-    'api_method' => 'taobao.item.get',
-    'require_params' => [
-        'fields',
-        'nick',
-        'iid'
-    ]
-);
+use base "Net::Top::Request::Base::Item::Get";
 
 1;
