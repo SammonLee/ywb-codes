6246e087008826a7cb87965401894496a86a794f
wenbinye <wenbinye@01fcc042-0321-11de-b556-9305e745af9e>
2009-12-05 10:48:19 +0000

    Revert "fix help"
    This reverts commit 98d4d8b25c0ba804559fc785fb9d309b6cdfb476.

--- perl/Farsail/demo/hello.pl
+++ perl/Farsail/demo/hello.pl
@@ -12,23 +12,22 @@ use lib "$Bin/../lib";
 use Farsail;
 use Data::Dumper qw(Dumper);
 use Log::Log4perl qw/:easy/;
-use YAML;
-use Data::Dumper qw(Dumper);
 Log::Log4perl->easy_init();
 
-my $f = Farsail->createInstance(%{YAML::Load(<<YML)});
-plugins: ['Farsail::Help', 'Farsail::Log']
-actions:
-  global:
-    hello:
-      args:
-        name: string
-config: { namespace: 'demo' }
-YML
-$f->dispatch();
+Farsail->createInstance(
+    'plugins' => ['Farsail::Help', 'Farsail::Log'],
+    'actions' => {
+        'global' => {
+            'hello' => {
+                'args' => {
+                    'name' => { type => 'string' }
+                }
+            }
+        }
+    }
+)->dispatch();
 
 sub ACTION_hello {
     my ($self, $farsail) = @_;
     print "Hello, " . $farsail->getArgs()->get('name', 'Farsail'), "!\n";
 }
-1;
