361baf5a71211bae50229ee95c4094637ba87837
wenbinye <wenbinye@01fcc042-0321-11de-b556-9305e745af9e>
2010-12-19 07:39:24 +0000

    romove old branch

--- branches/TOP_API/php/t/test_config.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once('simpletest/autorun.php');
-define('TOP_LIBPATH', realpath(dirname(__FILE__).'/../src/'));
-require( TOP_LIBPATH . DIRECTORY_SEPARATOR . 'Net/Top/Autoload.php');
-Net_Top_Autoload::register();
-
